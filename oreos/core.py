b58cbb26917a7488bb7c8728f4426744e91bb346
Kenneth Reitz <me@kennethreitz.com>
2011-11-19 15:22:04 -0500

    explicit for 2.5

--- oreos/core.py
+++ oreos/core.py
@@ -6,3 +6,20 @@ oreos.core
 
 The creamy white center.
 """
+
+from .structures import MultiDict
+from .monkeys import SimpleCookie
+
+
+def dict_from_string(s):
+    """Returns a MultiDict with Cookies."""
+
+    cookies = MultiDict()
+
+    c = SimpleCookie()
+    c.load(s)
+
+    for k,v in c.items():
+        cookies.add(k, v.value)
+
+    return cookies
